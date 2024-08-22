1c2fb780432c0fd8d1986891931b18b9e1682f74
Kazuma Sakaguchi <sakaguchi@KazumanoMacBook-Pro.local>
2024-08-22 20:33:24 +0900

    presenterのインターフェースを修正

--- internal/usecase/port/output/transaction_output.go
+++ internal/usecase/port/output/transaction_output.go
@@ -1,10 +1,10 @@
 package output
 
 import (
-	"github.com/LeoTwins/go-clean-architecture/internal/domain/model/transaction"
+	"github.com/LeoTwins/go-clean-architecture/internal/domain/model"
 	"github.com/LeoTwins/go-clean-architecture/internal/usecase/dto"
 )
 
 type ITransactionPresenter interface {
-	Output(transaction.Transaction) dto.TransactionOutput
+	Output(model.Transaction) dto.TransactionOutput
 }
