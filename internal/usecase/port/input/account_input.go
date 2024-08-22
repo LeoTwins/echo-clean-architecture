6409fc763d86aeb53a2bf8291562ce34c799fb74
Kazuma Sakaguchi <sakaguchi@KazumanoMacBook-Pro.local>
2024-08-22 20:29:16 +0900

    usecaseのインターフェース修正

--- internal/usecase/port/input/account_input.go
+++ internal/usecase/port/input/account_input.go
@@ -3,13 +3,12 @@ package input
 import (
 	"context"
 
-	"github.com/LeoTwins/go-clean-architecture/internal/domain/model/account"
-	"github.com/LeoTwins/go-clean-architecture/internal/domain/model/finance"
+	"github.com/LeoTwins/go-clean-architecture/internal/domain/model"
 )
 
 type IAccountUsecase interface {
-	OpenAccount(ctx context.Context, name string, initialDeposit finance.Money) (*account.Account, error)
-	Deposit(ctx context.Context, accountID uint, amount finance.Money) error
-	Withdraw(ctx context.Context, accountID uint, amount finance.Money) error
-	Transfer(ctx context.Context, fromAccountID uint, toAccountID uint, amount finance.Money) error
+	OpenAccount(ctx context.Context, name string, initialDeposit model.Money) (*model.Account, error)
+	Deposit(ctx context.Context, accountID uint, amount model.Money) error
+	Withdraw(ctx context.Context, accountID uint, amount model.Money) error
+	Transfer(ctx context.Context, fromAccountID uint, toAccountID uint, amount model.Money) error
 }
